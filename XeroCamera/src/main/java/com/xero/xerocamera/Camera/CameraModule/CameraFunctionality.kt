1f96d971ac9bf0450ffce570867cc10e9501c75c
Dhinesh <dhinesh.tech2001@gmail.com>
2024-09-15 09:34:25 +0530

    Video Capture has done

--- XeroCamera/src/main/java/com/xero/xerocamera/Camera/CameraModule/CameraFunctionality.kt
+++ XeroCamera/src/main/java/com/xero/xerocamera/Camera/CameraModule/CameraFunctionality.kt
@@ -8,40 +8,58 @@ import androidx.lifecycle.LifecycleOwner
 import com.xero.xerocamera.XeroCamera
 
 sealed class FlashMode {
-	data object FlashOn : FlashMode()
-	data object FlashOff : FlashMode()
-	data object FlashAuto : FlashMode()
-	data object TorchMode : FlashMode()
+  data object FlashOn : FlashMode()
+  data object FlashOff : FlashMode()
+  data object FlashAuto : FlashMode()
+  data object TorchMode : FlashMode()
 }
 
 sealed class LensFacing {
-	data object FrontFacing : LensFacing()
-	data object BackFacing : LensFacing()
+  data object FrontFacing : LensFacing()
+  data object BackFacing : LensFacing()
 }
 
 interface CameraFunctionality {
-	fun startCamera()
-	fun switchLensFacing(lensFacing: Int)
-	fun takePhoto(
-		onSuccess: ((imagePath: String) -> Unit)? = null,
-		onFailure: ((exception: Exception) -> Unit)? = null,
-		useCache: Boolean? = false,
-		directoryName: String? = "Demo",
-		fileName: String? = "img",
-		subDirectoryName: String? = "Photo",
-	)
+  fun startCamera()
+  fun switchLensFacing(lensFacing: Int)
+  fun takePhoto(
+	onSuccess: ((imagePath: String) -> Unit)? = null,
+	onFailure: ((exception: Exception) -> Unit)? = null,
+	useCache: Boolean? = false,
+	directoryName: String? = "Demo",
+	fileName: String? = "img",
+	subDirectoryName: String? = "Photo",
+  )
+  fun startVideo(
+	onStart: (() -> Unit?)? = null,
+	onSuccess: ((videoPath: String) -> Unit?)? = null,
+	onError: (() -> Unit?)? = null,
+	directoryName: String? = "Demo",
+	fileName: String? = "vid",
+	subDirectoryName: String? = "Video",
+  )
+  fun stopVideo()
+  fun pauseVideo()
+  fun resumeVideo()
+  fun photoCapture(
+	onSuccess: ((imagePath: String) -> Unit)? = null,
+	onFailure: ((exception: Exception) -> Unit)? = null,
+	directoryName: String? = "Demo",
+	fileName: String? = "img",
+	subDirectoryName: String? ="photo"
+  )
 
-	fun enableScanner(isScanner: Boolean)
-	fun resetScanning()
-	fun setFlashMode(flashMode: FlashMode)
-	fun setZoomRatio(@FloatRange(from = 0.0, to = 4.0) zoomRatio: Float)
-	fun setSeekBarZoom(seekBar: SeekBar, onStart: (() -> Unit)? = null, onStop: (() -> Unit)? = null)
+  fun enableScanner(isScanner: Boolean)
+  fun resetScanning()
+  fun setFlashMode(flashMode: FlashMode)
+  fun setZoomRatio(@FloatRange(from = 0.0, to = 4.0) zoomRatio: Float)
+  fun setSeekBarZoom(seekBar: SeekBar, onStart: (() -> Unit)? = null, onStop: (() -> Unit)? = null)
 
-	interface CompileTimeFunctionality {
-		fun setContext(context: Context): XeroCamera.Builder
-		fun setCameraPreview(cameraPreview: PreviewView): XeroCamera.Builder
-		fun setLifecycleOwner(owner: LifecycleOwner): XeroCamera.Builder
-		fun enableScanner(isScanner: Boolean): XeroCamera.Builder
-		fun setLensFacing(lensFacing : Int) : XeroCamera.Builder
-	}
+  interface CompileTimeFunctionality {
+	fun setContext(context: Context): XeroCamera.Builder
+	fun setCameraPreview(cameraPreview: PreviewView): XeroCamera.Builder
+	fun setLifecycleOwner(owner: LifecycleOwner): XeroCamera.Builder
+	fun enableScanner(isScanner: Boolean): XeroCamera.Builder
+	fun setLensFacing(lensFacing: Int): XeroCamera.Builder
+  }
 }
